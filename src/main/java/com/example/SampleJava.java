8f0182556961bef419be9a250c6febce2cce0739
Pradeep Chanumolu <pchanumolu@maprtech.com>
2015-03-31 01:04:48 -0700

    Adding Sample Java/Scala Files

---
+++ src/main/java/com/example/SampleJava.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by Pradeep on 3/31/15.
+ */
+
+public class SampleJava {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/scala/com/example/SampleScala.java
@@ -0,0 +1,8 @@
+package com.example
+
+/**
+ * Created by Pradeep on 3/31/15.
+ */
+class SampleScala {
+
+}
